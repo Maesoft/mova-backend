6724ffbe647fb276ed9a70cba99760c60f52ee16
Maesoft <matias_eguia@hotmail.com>
2026-04-28 11:44:50 -0300

    Fix: Eneble CORS

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
   app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true, // elimina campos extra
-    forbidNonWhitelisted: true, // error si mandan basura
-  }),
-);
+    new ValidationPipe({
+      whitelist: true, // elimina campos extra
+      forbidNonWhitelisted: true, // error si mandan basura
+    }),
+  );
 }
 bootstrap();
