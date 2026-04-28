6eb389142051e0ec4d0be127795fd8ce21d2c3ce
Maesoft <matias_eguia@hotmail.com>
2026-04-28 09:47:09 -0300

    Se agregaron Roles a Categorias y Ejercicios

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -13,6 +13,7 @@ import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/roles/roles.decorator';
 
 @Controller('categories')
 @UseGuards(JwtAuthGuard) // 🔐 opcional (recomendado si es admin)
@@ -20,21 +21,25 @@ export class CategoriesController {
   constructor(private readonly service: CategoriesService) {}
 
   @Post()
+  @Roles('trainer')
   create(@Body() dto: CreateCategoryDto) {
     return this.service.create(dto);
   }
 
   @Get()
+  @Roles('trainer')
   findAll() {
     return this.service.findAll();
   }
 
   @Get(':id')
+  @Roles('trainer')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.service.findOne(id);
   }
 
   @Patch(':id')
+  @Roles('trainer')
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() dto: UpdateCategoryDto,
@@ -43,6 +48,7 @@ export class CategoriesController {
   }
 
   @Delete(':id')
+  @Roles('trainer')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.service.remove(id);
   }
--- src/exercises/exercises.controller.ts
+++ src/exercises/exercises.controller.ts
@@ -13,6 +13,7 @@ import { ExercisesService } from './exercises.service';
 import { CreateExerciseDto } from './dto/create-exercise.dto';
 import { UpdateExerciseDto } from './dto/update-exercise.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/roles/roles.decorator';
 
 @Controller('exercises')
 @UseGuards(JwtAuthGuard)
@@ -20,21 +21,25 @@ export class ExercisesController {
   constructor(private readonly service: ExercisesService) {}
 
   @Post()
+  @Roles('trainer')
   create(@Body() dto: CreateExerciseDto) {
     return this.service.create(dto);
   }
 
   @Get()
+  @Roles('trainer')
   findAll() {
     return this.service.findAll();
   }
 
   @Get(':id')
+  @Roles('trainer')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.service.findOne(id);
   }
 
   @Patch(':id')
+  @Roles('trainer')
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() dto: UpdateExerciseDto,
@@ -43,6 +48,7 @@ export class ExercisesController {
   }
 
   @Delete(':id')
+  @Roles('trainer')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.service.remove(id);
   }
